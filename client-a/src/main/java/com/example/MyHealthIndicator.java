c22f65e2519b8401aae70c10b13d832edc656908
Stefan Eicher <stefan.eicher@gmail.com>
2016-12-13 23:48:08 +0100

    add flaky health indicator

---
+++ client-a/src/main/java/com/example/MyHealthIndicator.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MyHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check(); // perform some specific health check
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    private int check() {
+        //flaky health as example
+        return new Date().getSeconds()>30?2:0;
+    }
+
+}
